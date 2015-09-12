26bbeb754ab956ef6d317bab2eef682feeacaeb0
Ionuț Arțăriși <ionut@artarisi.eu>
2015-09-12 13:12:19 +0100

    test swaggerify with an actual flask app

--- tests/test_flask_classy_swagger.py
+++ tests/test_flask_classy_swagger.py
@@ -1,20 +1,35 @@
-from flask_classy_swagger import schema
+import json
+
+from flask import Flask
+
+from flask_classy_swagger import schema, swaggerify
+
+
+TITLE = 'MyTestAPI'
+VERSION = '0.9'
+BASIC_SCHEMA = {
+    'info': {
+        'title': TITLE,
+        'version': VERSION},
+    'swagger': '2.0',
+    'paths': {}}
 
 
 class TestSchema(object):
     def test_required_params(self):
-        assert schema('MyAPI', '1.0') == {
-            'info': {
-                'title': 'MyAPI',
-                'version': '1.0'},
-            'swagger': '2.0',
-            'paths': {}}
+        assert schema(TITLE, VERSION) == BASIC_SCHEMA
 
     def test_base_path(self):
-        assert schema('MyAPI', '1.0', base_path='/myswagger') == {
-            'info': {
-                'title': 'MyAPI',
-                'version': '1.0'},
-            'swagger': '2.0',
-            'paths': {},
-            'basePath': '/myswagger'}
+        assert (
+            schema(TITLE, VERSION, base_path='/myswagger') ==
+            dict(BASIC_SCHEMA, **{'basePath': '/myswagger'}))
+
+
+class TestSwaggerify(object):
+    def test_basic(self):
+        app = Flask('test')
+        swaggerify(app, TITLE, VERSION)
+        client = app.test_client()
+
+        response = client.get('/swagger')
+        assert json.loads(response.data) == BASIC_SCHEMA
