d1f51b59ea7edfce93ff90bfce29dedf4ef0588c
Ionuț Arțăriși <ionut@artarisi.eu>
2016-02-20 21:26:10 +0000

    Split integration tests into their own package

---
+++ tests/const.py
@@ -0,0 +1,9 @@
+TITLE = 'MyTestAPI'
+VERSION = '0.9'
+BASIC_SCHEMA = {
+    'info': {
+        'title': TITLE,
+        'version': VERSION},
+    'swagger': '2.0',
+    'paths': {},
+    'tags': []}
